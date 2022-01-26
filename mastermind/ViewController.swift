fd2328077b5bcf2164249dd0ba5f67e54e328fcf
Russell Mirabelli <russell@mirabellifamily.org>
2022-01-25 20:04:38 -0600

    Initial Commit

---
+++ mastermind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mastermind
+//
+//  Created by Russell Mirabelli on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
